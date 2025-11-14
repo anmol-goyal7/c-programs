65f52b9a7bdcd2b39bb788cc64385d97dced0a5b
Anmol Goyal <anmolafk7@gmail.com>
2025-11-15 00:43:08 +0530

    improved(still has bugs)
    it has a bug which i'm unable to configure, will fix it soon.

--- margincalculator.c
+++ margincalculator.c
@@ -1,20 +1,36 @@
 /* I've created this program so that I can calculate the number of classes that I can skip while maintaining my attendance.*/
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-	float a, b, margin, percentage;
-	printf("How much attendance do you need?\n Answer in percentage: \n");
+	float total_classes, absent_classes, present_classes;
+	float margin, percentage, required_ratio, total_classes_needed;
+	printf("How much attendance do you need?\nAnswer in percentage: \n");
 	scanf("%f",&percentage);
 
-	printf("Enter the number of classes you've attended: \n");
-	scanf("%f",&a);
+	printf("Enter the number of classes conducted: \n");
+	scanf("%f",&total_classes);
 	
 	printf("Enter the number of classes you were absent: \n");
-	scanf("%f",&b);
-	
-	margin= (a/(percentage/100))-(a+b);//This is the tricky part 
-	
-	printf("The total number of classes you can skip is:'%0.0f' \n", margin);
+	scanf("%f",&absent_classes);
+
+	if (percentage < 0 || percentage > 100){
+		printf("Enter the percentage between 0 to 100.");
+	return 1;
+	}
+
+	if (total_classes < 0){
+		printf("Number of classes cannot be less than 0.");
+	return 1;
+	}
+	if (absent_classes < 0){
+		printf("Absent classes cannot be less than 0.");
+	}
+	present_classes = total_classes - absent_classes;
+	required_ratio = percentage/100.0;
+	total_classes_needed = present_classes / required_ratio;
+	margin = total_classes_needed - total_classes;
+	printf("The total number of classes you can skip is:%.0f \n", floor(margin));
 	return 0;
 }
 
